8f11919c81bf5c10b045768efd020a21a87199fe
GreiceTreviso <medicoerro@gmail.com>
2026-05-19 18:00:35 -0300

    2

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
   return (
     <View style={styles.container}>
      
+   {/*inicio header*/}
+
    <view style={styles.cabeçalho}>
   
    <Feather name="menu" size={24} color="#FF0000" />
@@ -18,23 +21,33 @@ export default function App() {
 
       </TouchableOpacity>
 
-   <view style={styles.containerBusca}>
+   <View style={styles.containerBusca}>
     <TextInput
       placeholder='digite o filme que desejar'
+      style = {styles.inputSearch}
     ></TextInput>
 
 
+    <TouchableOpacity>
+    <Ionicons name="search-circle-sharp" size={24} color="black" />
+    </TouchableOpacity>
+</View>
 
+ <View>
+{/*inicio do banner*/}
 
+    <Text style={styles.textao}> Em Cartaz </Text>
 
-   </view>
+    <Image
+    source={require("./assets/melhor filme poster.jpg")}
+    style={styles.imageBanner}
+    />
+    <Image
+    source={require("./assets/melhor q iron lung.jpg")}
+    style={styles.imageBanner}
+    />
 
-
-
-
-
-
-
+</View>
 
 
 
@@ -74,6 +87,36 @@ const styles = StyleSheet.create({
     width:'90%',
     backgroundColor:'white',
     borderRadius:5,
-    padding:8
+    padding:8,
+    flexDirection: 'row',
+    justifyContent: "space-between",
+  },
+  inputSearch: {
+    height: 10,
+    padding: 8,
+    width: '100%'
+  },
+
+  imageBanner: {
+    width: '250%',
+    height: 250,
+    marginTop: 15,
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    borderRadius: 10
+  },
+
+  textao: {
+    color: '#FF0000',
+    width: '90%',
+    fontSize:20,
+    marginTop:15,
+    fontWeight:'bold',
+    marginBottom: 15
   }
+
+
+
+
+
 });
