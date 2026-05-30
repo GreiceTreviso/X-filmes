bd35431f2ad71a80ffa4102a9ae6d00a9dfbeb85
GreiceTreviso <medicoerro@gmail.com>
2026-05-30 16:32:42 -0300

    6 commit

--- movies.js
+++ movies.js
@@ -1,3 +1,61 @@
-const data [ {
-      id
-}]
+const data = [ {
+      id: '1',
+      nome:'8,2',
+      nota: 'Clockwork Orange',
+      Image: 'imagens/orange.jpg'
+},
+ {
+      id: '2',
+      nome:'Interestelar',
+      nota: '8,7',
+      Image: 'imagens/interestelar.webp'
+},
+{
+      id: '3',
+      nome:'The Amazing Digital Circus',
+      nota: '10',
+      Image: 'imagens/tadc.webp'
+},
+{
+      id: '4',
+      nome:'Sonic 3',
+      nota: '6,9',
+      Image: 'imagens/sonic 3.jpg'
+},
+{
+      id: '5',
+      nome:'Super Mario Galaxy',
+      nota: ' ̶6̶,̶3̶  10',
+      Image: 'imagens/melhor filme 2026.jpeg'
+},
+{
+      id: '6',
+      nome:'Iron Lung',
+      nota: '0',
+      Image: 'imagens/melhor filme poster.jpg'
+},
+{
+      id: '7',
+      nome:'Joker',
+      nota: '8,3',
+      Image: 'imagens/melhor q iron lung.jpg'
+},
+{
+      id: '8',
+      nome:'Homem Aranha no Aranhaverso',
+      nota: '8,4',
+      Image: 'imagens/homem aranha miles morales.jpg'
+},
+{
+      id: '9',
+      nome:'Batman O cavaleiro das Trevas',
+      nota: '9,1',
+      Image: 'imagens/batman 2.webp'
+},
+{
+      id: '10',
+      nome:'Batman',
+      nota: '7,8',
+      Image: 'imagens/batman.jpg'
+},
+]
