42c7b825156d0f970c8d955efb8258f89e8db2c5
GreiceTreviso <medicoerro@gmail.com>
2026-06-27 02:05:19 -0300

    nono commit
    pequenos ajustes

--- App.js
+++ App.js
@@ -17,18 +17,20 @@ export default function App(){
    <Search></Search>
    <Banner></Banner>
   
-    <View style= {{width: '90%'}}> 
+    <View style= {{width: '90%', marginTop: '8%',  }}> 
       <FlatList
       horizontal= {true}
       data={filmes}
       keyExtractor={(item)=> item.id}
+      ItemSeparatorComponent={() => <View style={{ width: 8 }} />}
+      //separador entre itens da flatList, dando espaço entre eles
       renderItem={({item}) => ( 
 
-        <TouchableOpacity>
-          <Image style = {{width:80,height: 100}}source = {{uri: item.Image}}></Image>
-          <Text > {item.nome} </Text>
+        <TouchableOpacity >
+          <Image style = {{width:120,height: 180}}source = {{uri: item.Image}}></Image>
+          <Text style={{ width: 120, textAlign: 'center', marginTop: '5', }} numberOfLines={2}> {item.nome} </Text>
         </TouchableOpacity>
-        
+        // width pra defi largura texto e numberOflines diz quantas linhas ocupar
 
 
 
--- src/components/banner/styles.js
+++ src/components/banner/styles.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
         fontSize:20,
         marginTop:15,
         fontWeight:'bold',
-        marginBottom: 15
+        marginBottom: 2
       }
     
 
